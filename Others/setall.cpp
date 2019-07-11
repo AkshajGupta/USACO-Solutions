ba55088ddc59183e6cdbd46e9f99851ede7174ff
Akshaj Gupta <50127724+AkshajGupta@users.noreply.github.com>
2019-07-10 22:01:00 -0700

    Create setall.cpp

---
+++ Others/setall.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+int n, size, arr[10001], digits[10001], counter=0;
+void gen(int index){
+    if(index==size){
+        for(int i=0; i<size; i++){
+            cout << arr[i];
+        }
+        cout << endl;
+        return;
+    }
+    else{
+        for(int i=0; i<n; i++){
+            arr[index]=digits[i];
+            gen(index+1);
+        }
+    }
+    return;
+}
+int main(){
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> digits[i];
+    }
+    cin >> size;
+    gen(0);
+}
