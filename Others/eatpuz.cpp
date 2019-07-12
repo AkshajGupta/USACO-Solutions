77909b051342294444a85c6880b5056384fcc97c
Akshaj Gupta <50127724+AkshajGupta@users.noreply.github.com>
2019-07-11 18:53:46 -0700

    Update eatpuz.cpp

--- Others/eatpuz.cpp
+++ Others/eatpuz.cpp
@@ -1,54 +1,38 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
 using namespace std;
+int n, m, size=1, arr[1001], digits[1001], counter=0, total=0, maxx=-10000000;
+void gen(int index, int lastindex){
+    if(index==size){
+        for(int i=0; i<size; i++){
+            total+=arr[i];
+            // cout << total << endl;
+            if(total<n and total>maxx){
+                maxx=total;
+            }
+        }
+        total=0;
+        return;
+    }
+    else{
+        for(int i=lastindex; i<m; i++){
+            arr[index]=digits[i];
+            gen(index+1, i+1);
+        }
+    }
+    return;
+}
 int main(){
-    int n, a, temp, temp2, answer=0;
-    cin >> n >> a;
-    int arr[a];
-    for(int i=0; i<a; i++){
-        cin >> arr[i];
+    cin >> n >> m;
+    for(int i=0; i<m; i++){
+        cin >> digits[i];
     }
-    for(int i=0; i<a-1; i++){
-        temp2=i;
-        while(arr[i]>arr[i+1]){
-            temp=arr[i];
-            arr[i]=arr[i+1];
-            arr[i+1]=temp;
-            i--;
-        }
-        i=temp2;
+    for(int i=0; i<m; i++){
+        size=i+1;
+        gen(0,0);
     }
-    for(int i=0; i<a; i++){
-        for(int j=0; j<a; j++){
-            if(arr[j]+arr[i]<=n and arr[j]+arr[i]>answer and j!=i){
-                answer=arr[i]+arr[j];
-            }
-            if(arr[j]+arr[i]>n){
-                break;
-            }
-        }
-    }
-    for(int i=0; i<a; i++){
-        for(int j=0; j<a; j++){
-            for(int t=0; t<a; t++){
-                if(i!=j and i!=t and t!=j and arr[i]+arr[j]+arr[t]>answer and arr[i]+arr[j]+arr[t]<=n){
-                    answer = arr[i]+arr[j]+arr[t];
-                }
-                if(arr[i]+arr[j]+arr[t]>n){
-                    break;
-                }
-            }
-        }
-    }
-    for(int i=0; i<a; i++){
-        for(int j=0; j<a; j++){
-            for(int t=0; t<a; t++){
-                for(int f=0; f<a; f++){
-                    if(i!=j and i!=t and t!=j and i!=f and j!=f and t!=f and arr[i]+arr[j]+arr[t]+arr[f]>answer and arr[i]+arr[j]+arr[t]+arr[f]<=n){
-                    answer = arr[i]+arr[j]+arr[t]+arr[f];
-                }
-                }
-            }
-        }
-    }
-    cout << answer << endl;
+    cout << maxx << endl;
 }
